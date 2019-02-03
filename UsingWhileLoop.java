fa860cfe1d284e669699d34024ff9cc0235156c0
jentimonera <jentimonera1@gmail.com>
2019-02-03 16:44:20 +0800

    While Loop that prints 0 to 9, skipping 4

---
+++ UsingWhileLoop.java
@@ -0,0 +1,20 @@
+public class UsingWhileLoop{
+
+	public static void main(String[] args) {
+
+		int x = 0;
+		while (x<10) 
+		{
+			if (x == 4){
+				x++;
+				continue;
+		}
+			System.out.println(x);
+			x++;
+			
+}	
+	
+}
+
+}
+
