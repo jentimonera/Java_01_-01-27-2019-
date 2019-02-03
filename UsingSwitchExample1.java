4ff2670d3784cc801b9855bc9d456107f76033f4
jentimonera <jentimonera1@gmail.com>
2019-02-03 12:51:14 +0800

    Program using the switch example

---
+++ UsingSwitchExample1.java
@@ -0,0 +1,24 @@
+public class UsingSwitchExample1 {
+
+	public static void main(String[] args)
+	{
+		int grade = 80;
+		switch (grade){
+			case 200:
+				System.out.println ("Excellent!");
+				break;
+			case 90:
+				System.out.println ("Good Job!");
+				break;
+			case 80:
+				System.out.println ("Study Harder");
+				break;
+			default:
+				System.out.println ("Sorry, you failed!");
+		}
+	}
+		
+}
+
+
+		
